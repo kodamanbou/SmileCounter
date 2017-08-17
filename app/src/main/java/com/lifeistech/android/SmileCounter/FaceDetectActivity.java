53db0ec472b95db1c48ac515e747ffb7c33d4d66
kodamanbou <kodamanbou0424@gmail.com>
2017-08-18 04:19:08 +0900

    Initial commit

---
+++ app/src/main/java/com/lifeistech/android/SmileCounter/FaceDetectActivity.java
@@ -0,0 +1,162 @@
+package com.lifeistech.android.SmileCounter;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Environment;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.face.Face;
+import com.google.android.gms.vision.face.FaceDetector;
+import com.lifeistech.android.SmileCounter.R;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class FaceDetectActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private TextView textView;
+    int score = 0;
+    Bitmap bitmap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face_detect);
+
+        imageView = (ImageView) findViewById(R.id.detectImage);
+        textView = (TextView) findViewById(R.id.point);
+
+        Paint myRectPaint = new Paint();
+        myRectPaint.setStrokeWidth(5);
+        myRectPaint.setColor(Color.RED);
+        myRectPaint.setStyle(Paint.Style.STROKE);
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inMutable = true;
+
+        //読み込んだ画像をビットマップに変換する------FileProvider を使う
+
+        File file = new File(Environment.getExternalStorageDirectory() + "/SmileCounter" + "/camera_test.jpg");
+
+        Uri bitUri = FileProvider.getUriForFile(this, "com.lifeistech.android.SmileCounter" + ".fileprovider", file);
+
+        Log.d("Bitmap_Test", bitUri.getPath());
+
+        try {
+            // 71行目でstreamがnullになってしまう
+            InputStream stream = getContentResolver().openInputStream(bitUri);
+            bitmap = BitmapFactory.decodeStream(new BufferedInputStream(stream));
+            stream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (bitmap == null) {
+            Log.d("Bitmap_null", "NULL!!");
+        } else {
+            Log.d("Bitmap_null", "NON NULL!!");
+        }
+
+        Bitmap tempBitmap = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.RGB_565);
+        Canvas tempCanvas = new Canvas(tempBitmap);
+        tempCanvas.drawBitmap(bitmap, 0, 0, null);
+
+        FaceDetector detector = new FaceDetector.Builder(this)
+                .setTrackingEnabled(false)
+                .setLandmarkType(FaceDetector.ALL_LANDMARKS)
+                .setClassificationType(FaceDetector.ALL_CLASSIFICATIONS)
+                .build();
+
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+        SparseArray<Face> faces = detector.detect(frame);
+
+        for(int i=0; i<faces.size(); i++) {
+            int j = 0;
+            Face thisFace = faces.valueAt(i);
+            float x1 = thisFace.getPosition().x;
+            float y1 = thisFace.getPosition().y;
+            float x2 = x1 + thisFace.getWidth();
+            float y2 = y1 + thisFace.getHeight();
+            //笑顔かどうかを示す小数値
+            float smilingProbability = thisFace.getIsSmilingProbability();
+            //TODO 笑顔係数に応じてスコアを加算
+            if (smilingProbability > 0.3f && smilingProbability <= 0.5f) {
+                score = score + 5;
+                j++;
+                if (j >= 3) {
+                    score *= 2;
+                }
+            } else if (smilingProbability > 0.5f && smilingProbability <= 0.7f) {
+                score = score + 7;
+                j++;
+                if (j >= 3) {
+                    score *= 2;
+                }
+            } else if (smilingProbability > 0.7f) {
+                score = score + 10;
+                j++;
+                if (j >= 3) {
+                    score *= 2;
+                }
+            }
+
+            tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, myRectPaint);
+        }
+        imageView.setImageDrawable(new BitmapDrawable(getResources(), tempBitmap));
+        textView.setText(String.valueOf(score) + "pt");
+
+    }
+
+    public void backToTitle(View v) {
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] bytes = baos.toByteArray();
+
+        Date date = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmss", Locale.JAPAN);
+        String s = sdf.format(date);
+
+        String bitmapPath = "";
+        try {
+            bitmapPath = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".fileprovider", new File(Environment.getExternalStorageDirectory().getPath())).getPath();
+
+            FileOutputStream fos = null;
+            fos = new FileOutputStream(bitmapPath + "/smilecounter_" + s + ".jpg");
+            fos.write(bytes);
+            fos.flush();
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Intent intent = new Intent(this, IndexActivity.class);
+        intent.putExtra("SCORE", score);
+        intent.putExtra("SCORE_IMAGE", bitmapPath + "/smilecounter_" + s + ".jpg");
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/lifeistech/android/SmileCounter/IndexActivity.java
@@ -0,0 +1,264 @@
+package com.lifeistech.android.SmileCounter;
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.lifeistech.android.SmileCounter.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+public class IndexActivity extends AppCompatActivity {
+
+    private final String PREF_KEY = "DataSave";
+
+    //TODO 記録の表示及びその他の機能追加
+    int score;
+    private TextView textView;
+    private Button button;
+    private ImageView imageView;
+    private ImageView galleryView;
+    private Button lookButton;
+    private Button clearButton;
+    private GestureDetector gestureDetector;
+    private TextView labelTextView;
+    private TextView numberTextView;
+
+    private ListIterator<String> iterator;
+    private ArrayList<String> fileNames;
+
+    private boolean isLooking = false;
+    private boolean isChanged = false;
+    private String sdPath = "";
+
+    //フリックを検知するための基準
+    private static final int SWIPE_MAX_OFF_PATH = 250;
+    private static final int SWIPE_MIN_DISTANCE = 50;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+    static final int REQUEST_CAPTURE_IMAGE = 100;
+
+    static Uri selectedImageUri = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+
+        textView = (TextView) findViewById(R.id.score);
+        button = (Button) findViewById(R.id.takePicture);
+        imageView = (ImageView) findViewById(R.id.bestImage);
+        lookButton = (Button) findViewById(R.id.lookPicture);
+        labelTextView = (TextView) findViewById(R.id.textView);
+        numberTextView = (TextView) findViewById(R.id.numberText);
+
+        numberTextView.setTypeface(Typeface.createFromAsset(getAssets(), "JiyunoTsubasa.ttf"));
+        textView.setTypeface(Typeface.createFromAsset(getAssets(), "JiyunoTsubasa.ttf"));
+        labelTextView.setTypeface(Typeface.createFromAsset(getAssets(), "JiyunoTsubasa.ttf"));
+
+        gestureDetector = new GestureDetector(this, gestureListener);
+
+        //FaceDetectActivityからスコアを取得する
+        Intent intent = getIntent();
+        score = intent.getIntExtra("SCORE", 0);
+        String bitPath = intent.getStringExtra("SCORE_IMAGE");
+        Bitmap bitmap = null;
+
+        if (bitPath != null) {
+            bitmap = BitmapFactory.decodeFile(bitPath);
+        }
+
+        SharedPreferences data = getSharedPreferences(PREF_KEY, Context.MODE_PRIVATE);
+        int highestScore = data.getInt("Score", 0);
+        String bitString = data.getString("ScoreImage", "");
+
+        if (highestScore < score) {
+            SharedPreferences.Editor editor = data.edit();
+            editor.putInt("Score", score);
+            highestScore = score;
+            isChanged = true;
+            editor.putString("ScoreImage", bitPath);
+            editor.apply();
+
+            if (bitPath != null) {
+                imageView.setImageBitmap(bitmap);
+            }
+        } else if (highestScore == score && !(isChanged)) {
+            if (!bitString.equals("")) {
+                bitPath = data.getString("ScoreImage", "");
+                bitmap = BitmapFactory.decodeFile(bitPath);
+                imageView.setImageBitmap(bitmap);
+            } else {
+                bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.droidkun);
+                imageView.setImageBitmap(bitmap);
+            }
+        } else if (highestScore > score) {
+            imageView.setImageBitmap(BitmapFactory.decodeFile(bitString));
+            SharedPreferences.Editor editor = data.edit();
+            editor.putString("ScoreImage", bitString);
+            editor.apply();
+        }
+
+        textView.setText(String.valueOf(highestScore) + "pt");
+
+        if (highestScore >= 20 && highestScore < 30) {
+            textView.setTextColor(Color.BLUE);
+        } else if (highestScore >= 30 && highestScore < 40) {
+            textView.setTextColor(Color.GREEN);
+        } else if (highestScore >= 40 && highestScore < 50) {
+            textView.setTextColor(Color.YELLOW);
+        } else if (highestScore >= 50 && highestScore < 60) {
+            textView.setTextColor(Color.RED);
+        }
+
+    }
+
+    public void picture(View v) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //TODO FileProvider で保存・参照する
+        File file = new File(Environment.getExternalStorageDirectory(), "/SmileCounter" + "camera_test.jpg");
+
+        if(!file.exists()) {
+            file.mkdirs();
+        }
+
+        selectedImageUri = FileProvider.getUriForFile(this, "com.lifeistech.android.SmileCounter" + ".fileprovider" , file);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, selectedImageUri);
+        startActivityForResult(intent, REQUEST_CAPTURE_IMAGE);
+    }
+
+    public void look(View v) {
+
+        isLooking = true;
+
+        fileNames = new ArrayList<>();
+        sdPath = Environment.getExternalStorageDirectory().getPath() + "/SmileCounter/";
+        File[] files = new File(FileProvider.getUriForFile(this, "com.lifeistech.android.SmileCounter" + ".fileprovider", new File(Environment.getExternalStorageDirectory().getPath() + "/SmileCounter/")).getPath()).listFiles();
+
+        if (files != null) {
+            Log.d("JPG_DIRECTORY", "" + files[0].toString());
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].isFile() && files[i].getName().endsWith(".jpg")) {
+                    fileNames.add(files[i].getName());
+                    Log.d("JPG_DIRECTORY", fileNames.get(i));
+                }
+            }
+        }
+
+        if (fileNames.size() > 0) {
+
+            iterator = fileNames.listIterator();
+
+            galleryView = (ImageView) findViewById(R.id.gallery);
+            clearButton = (Button) findViewById(R.id.clear_button);
+
+            if (iterator.hasNext()) {
+                galleryView.setImageBitmap(BitmapFactory.decodeFile(sdPath + iterator.next()));
+                numberTextView.setText(String.valueOf(iterator.nextIndex()) + "/" + fileNames.size());
+            }
+
+            textView.setVisibility(View.GONE);
+            labelTextView.setVisibility(View.GONE);
+            button.setVisibility(View.GONE);
+            imageView.setVisibility(View.GONE);
+            lookButton.setVisibility(View.GONE);
+            clearButton.setVisibility(View.VISIBLE);
+            galleryView.setVisibility(View.VISIBLE);
+            numberTextView.setVisibility(View.VISIBLE);
+
+            Toast.makeText(this, "スワイプして写真を見る", Toast.LENGTH_LONG).show();
+
+        } else {
+            Toast.makeText(this, "写真がありません！", Toast.LENGTH_LONG).show();
+            isLooking = false;
+        }
+    }
+
+    public void clear(View v) {
+        galleryView.setVisibility(View.GONE);
+        textView.setVisibility(View.VISIBLE);
+        labelTextView.setVisibility(View.VISIBLE);
+        button.setVisibility(View.VISIBLE);
+        imageView.setVisibility(View.VISIBLE);
+        lookButton.setVisibility(View.VISIBLE);
+        clearButton.setVisibility(View.GONE);
+        numberTextView.setVisibility(View.GONE);
+    }
+
+    private final GestureDetector.SimpleOnGestureListener gestureListener = new GestureDetector.SimpleOnGestureListener() {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            try {
+
+                if (!isLooking) {
+                    return false;
+                }
+
+                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH) {
+                    return false;
+                } else if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    // 右から左
+                    if (iterator.hasNext()) {
+                        numberTextView.setText(String.valueOf(iterator.nextIndex() + 1) + "/" + fileNames.size());
+                        galleryView.setImageBitmap(BitmapFactory.decodeFile(sdPath + iterator.next()));
+                    } else {
+                        //TODO 行き止まりアニメーション
+                    }
+                    return true;
+                } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    // 左から右
+                    if (iterator.hasPrevious()) {
+                        numberTextView.setText(String.valueOf(iterator.previousIndex() + 1) + "/" + fileNames.size());
+                        galleryView.setImageBitmap(BitmapFactory.decodeFile(sdPath + iterator.previous()));
+                    } else {
+                        //TODO 行き止まりアニメーション
+                    }
+                    return true;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return false;
+        }
+    };
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return gestureDetector.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (REQUEST_CAPTURE_IMAGE == requestCode
+                && resultCode == Activity.RESULT_OK) {
+            Intent intent = new Intent(this, FaceDetectActivity.class);
+            startActivity(intent);
+        }
+    }
+}
